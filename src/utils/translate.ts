f3245dcfc44c56d182eadb5a0d1dd4bcc6362067
END-i <evgeniy.donev@gmail.com>
2020-05-22 16:40:59 +0300

    translate

---
+++ src/utils/translate.ts
@@ -0,0 +1,35 @@
+import { useGlobalState } from "./context";
+
+type ILanguages = {
+  [key: string]: {
+    [key: string]: string;
+  };
+};
+
+const languages: ILanguages = {
+  search: {
+    en: "Search",
+    ru: "Поиск",
+  },
+  more: {
+    en: "Show 20 more",
+    ru: "Показать еще 20",
+  },
+  "no matched": {
+    en: "There are no movies that matched your query.",
+    ru: "Нет фильмов, соответствующих вашему запросу.",
+  },
+  "start search": {
+    en: "Enter the name of the movie in the search field to start the.",
+    ru: "Введите название фильма в поле поиска, чтобы начать.",
+  },
+  tokyo: {
+    en: "Tokyo",
+    ru: "Токио",
+  },
+};
+
+export default function (str: string) {
+  const { lang } = useGlobalState();
+  return languages[str][lang.key];
+}
