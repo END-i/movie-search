cac5a41ac4dd2abd543ae05064a8d7d680dba691
END-i <evgeniy.donev@gmail.com>
2020-05-22 16:41:21 +0300

    export components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Dashboard } from "./dashboard";
+export { default as Header } from "./header";
+export { default as Films } from "./films";
+export { default as Footer } from "./footer";
