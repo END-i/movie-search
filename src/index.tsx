f8c525984ae3e2c5b0844d2527087535531d2e8a
END-i <evgeniy.donev@gmail.com>
2020-06-03 18:15:19 +0300

    fix app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./app";
+import { createBrowserHistory } from "history";
+import { Router as BrowserRouter } from "react-router";
+
+import Routes from "pages";
+import { Provider } from "utils/context";
 import "./style.css";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter history={history}>
+      <Provider>
+        <Routes />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root"),
 );
